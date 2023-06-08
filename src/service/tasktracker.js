85fe442ae4b4f1dfa293901c222d509e560e788b
Souradeep Ray <souradeepray@Souradeeps-MacBook-Pro.local>
2023-06-08 20:31:01 +0530

    Fixing code

--- src/service/tasktracker.js
+++ src/service/tasktracker.js
@@ -1,9 +1,12 @@
 const dbExecutor = require('../database/dbExecutor')
 
 async function createTask(req, res)  {
-    const task = req.body;
-    const val = dbExecutor.createTask(req);
-    res.send(`Create Task result is : ${JSON.stringify(val)}`);
+    try {
+        const val = await dbExecutor.createTask(req);
+        res.send({status: "success", data : val, msg : "Successfully created task."});
+    } catch (error) {
+        res.send({status: "error", msg: `Fetched error while creating tasks. ${error}`})
+    }
 }
 
 async function getAllTask(req, res)  {
@@ -11,7 +14,7 @@ async function getAllTask(req, res)  {
         const val = await dbExecutor.getAllTask();
         res.send({status: "success", data : val, msg : "Successfully fetched tasks."});
     } catch (error) {
-        res.send({status: "error", msg: `Fetched error while getting task. ${error}`})
+        res.send({status: "error", msg: `Fetched error while getting all tasks. ${error}`})
     }
 }
 
@@ -28,20 +31,20 @@ async function getTaskById(req, res)  {
 async function updateTaskById(req, res)  {
     try {
         const id = req.params.id;
-        const val = await dbExecutor.getTaskById(id);
+        const val = await dbExecutor.updateTaskById(id, req);
         res.send({status: "success", data : val, msg : "Successfully fetched tasks."});
     } catch (error) {
-        res.send({status: "error", msg: `Fetched error while getting task. ${error}`})
+        res.send({status: "error", msg: `Fetched error while updating task. ${error}`})
     }
 }
 
 async function deleteTaskById(req, res)  {
     try {
         const id = req.params.id;
-        const val = await dbExecutor.getTaskById(id);
+        const val = await dbExecutor.deleteTaskById(id);
         res.send({status: "success", data : val, msg : "Successfully fetched tasks."});
     } catch (error) {
-        res.send({status: "error", msg: `Fetched error while getting task. ${error}`})
+        res.send({status: "error", msg: `Fetched error while deleting task. ${error}`})
     }
 }
 
