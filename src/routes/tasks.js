278e9c96e3a82657cb1ffa98eab4d25cd6203105
Souradeep Ray <souradeepray@Souradeeps-MacBook-Pro.local>
2023-06-08 20:18:10 +0530

    Adding functionality

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const router = express.Router();
-const {createTask, getTask} = require('../service/tasktracker')
+const {createTask, getAllTask, getTaskById} = require('../service/tasktracker')
 
 router.post('/createTask' , (req, res) => {
     createTask(req, res);
 })
 
-router.get("/getTask", (req, res) => {
-    getTask(req, res);
+router.get("/getAllTask", (req, res) => {
+    getAllTask(req, res);
 })
 
+router.get("/getTask/:id", (req, res) => {
+    getTaskById(req, res);
+})
+
+router.get("/getTask/:id", (req, res) => {
+    getTaskById(req, res);
+})
+
+router.put("/updateTask/:id", (req, res) => {
+    updateTaskById(req, res);
+})
+
+router.delete("/deleteTask/:id", (req, res) => {
+    updateTaskById(req, res);
+})
 
 module.exports = router;
--- src/service/tasktracker.js
+++ src/service/tasktracker.js
@@ -6,13 +6,49 @@ async function createTask(req, res)  {
     res.send(`Create Task result is : ${JSON.stringify(val)}`);
 }
 
-async function getTask(req, res)  {
+async function getAllTask(req, res)  {
     try {
-        const val = await dbExecutor.getTask();
+        const val = await dbExecutor.getAllTask();
         res.send({status: "success", data : val, msg : "Successfully fetched tasks."});
     } catch (error) {
         res.send({status: "error", msg: `Fetched error while getting task. ${error}`})
     }
 }
 
-module.exports = {createTask, getTask};
+async function getTaskById(req, res)  {
+    try {
+        const id = req.params.id;
+        const val = await dbExecutor.getTaskById(id);
+        res.send({status: "success", data : val, msg : "Successfully fetched tasks."});
+    } catch (error) {
+        res.send({status: "error", msg: `Fetched error while getting task. ${error}`})
+    }
+}
+
+async function updateTaskById(req, res)  {
+    try {
+        const id = req.params.id;
+        const val = await dbExecutor.getTaskById(id);
+        res.send({status: "success", data : val, msg : "Successfully fetched tasks."});
+    } catch (error) {
+        res.send({status: "error", msg: `Fetched error while getting task. ${error}`})
+    }
+}
+
+async function deleteTaskById(req, res)  {
+    try {
+        const id = req.params.id;
+        const val = await dbExecutor.getTaskById(id);
+        res.send({status: "success", data : val, msg : "Successfully fetched tasks."});
+    } catch (error) {
+        res.send({status: "error", msg: `Fetched error while getting task. ${error}`})
+    }
+}
+
+module.exports = {
+    createTask, 
+    getAllTask, 
+    getTaskById,
+    updateTaskById,
+    deleteTaskById
+};
